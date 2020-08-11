3cdfcee10bf45b1f58f44377fb10a34e4bf9b9a3
Squirtle <tienhoang.pham@bc.fi>
2020-08-11 10:42:28 +0300

    Add Login and Register components

--- src/Components/Auth/LogIn.js
+++ src/Components/Auth/LogIn.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 const LogIn = () => (
-    <form></form>
+    <div className="container">
+        <form>
+            <div className="input-field">
+                <input id="email" type="text" class="validate"/>
+                <label for="email">Email:</label>
+            </div>
+            <div className="input-field">
+                <input id="password" type="password" class="validate"/>
+                <label for="password">Password</label>
+            </div>
+            <button class="btn waves-effect waves-light" type="submit" name="action">Log In</button>
+        </form>
+    </div>
 )
 
 export default LogIn;
--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
 const Register = () => (
-    <form></form>
+    <div className="container">
+        <form>
+            <div className="input-field">
+                <input id="firstname" type="text" className="validate"/>
+                <label for="firstname">First Name:</label>
+            </div>
+            <div className="input-field">
+                <input id="lastname" type="text" className="validate"/>
+                <label for="lastname">Last Name:</label>
+            </div>
+            <div className="input-field">
+                <input id="email" type="text" className="validate"/>
+                <label for="email">Email:</label>
+            </div>
+            <div className="input-field">
+                <input id="password" type="password" className="validate"/>
+                <label for="password">Password</label>
+            </div>
+            <button className="btn waves-effect waves-light" type="submit" name="action">Register</button>
+        </form>
+    </div>
 )
 
 export default Register;
