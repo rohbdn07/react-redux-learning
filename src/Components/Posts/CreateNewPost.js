9296939f69655e0d123d587a0c3fabd382250acd
Squirtle <tienhoang.pham@bc.fi>
2020-08-11 10:46:48 +0300

    Add create new post component

--- src/Components/Posts/CreateNewPost.js
+++ src/Components/Posts/CreateNewPost.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const CreateNewPost = () => (
-    <form></form>
+    <div className="container">
+        <form>
+            <div className="input-field">
+                <input id="postTitle" type="text" className="validate"/>
+                <label for="postTitle">Title:</label>
+            </div>
+            <div className="input-field">
+                <textarea id="postContent" class="materialize-textarea" data-length="500"></textarea>
+                <label for="postContent">Content:</label>
+            </div>
+
+            <button className="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+        </form>
+    </div>
 )
 
 export default CreateNewPost;
