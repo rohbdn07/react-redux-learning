061260d67584f9bee52a5d61ee4dc67632305a78
Squirtle <tienhoang.pham@bc.fi>
2020-08-12 10:40:09 +0300

    ignore configuration files

--- src/App.js
+++ src/App.js
@@ -9,29 +9,19 @@ import Register from './Components/Auth/Register'
 import PostDetails from './Components/Posts/PostDetails'
 import CreateNewPost from './Components/Posts/CreateNewPost'
 import Firebase from 'firebase'
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyDqGbuqhLFY8vETEI0vIvsIuTlu-b-ss3E",
-  authDomain: "sample-project-63d7a.firebaseapp.com",
-  databaseURL: "https://sample-project-63d7a.firebaseio.com",
-  projectId: "sample-project-63d7a",
-  storageBucket: "sample-project-63d7a.appspot.com",
-  messagingSenderId: "366181844068",
-  appId: "1:366181844068:web:e88e4c997325415eaf8b71",
-  measurementId: "G-DTFXDW9NBV"
-};
+import { FIREBASE_CONFIG as firebaseConfig } from './config/firebaseConfig';
 
 // Initialize Firebase
 Firebase.initializeApp(firebaseConfig);
 Firebase.analytics();
 
 const db = Firebase.firestore();
+
 db.collection('posts').get()
   .then(resp => {
     console.log('resp is: ');
     console.log(resp);
-    console.log('resp.docs is: ' + resp.docs);
+    console.log('resp.docs is: ');
     console.log(resp.docs);
     console.log('resp.docs[0].data()');
     console.log(resp.docs[0].data());
