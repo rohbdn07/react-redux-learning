1050e16882e05e97025f13f33167fb8691361d18
Squirtle <tienhoang.pham@bc.fi>
2020-08-18 10:47:16 +0300

    Add createPost action

--- src/Components/Posts/CreateNewPost.js
+++ src/Components/Posts/CreateNewPost.js
@@ -1,20 +1,59 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { createPost } from '../../store/actions/postActions'
 
-const CreateNewPost = () => (
-    <div className="container">
-        <form>
-            <div className="input-field">
-                <input id="postTitle" type="text" className="validate"/>
-                <label for="postTitle">Title:</label>
+class CreateNewPost extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+        this.handleChange = this.handleChange.bind(this);
+        this.handlSubmission = this.handlSubmission.bind(this);
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmission = (e) => {
+        e.preventDefault();
+        this.props.createPost({
+            title: this.state.postTitle,
+            content: this.state.postContent
+        })
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <form onSubmit={this.handleSubmission}>
+                    <div className="input-field">
+                        <input id="postTitle" type="text" className="validate" onChange={this.handleChange}/>
+                        <label for="postTitle">Title:</label>
+                    </div>
+                    <div className="input-field">
+                        <textarea id="postContent" className="materialize-textarea" data-length="500" onChange={this.handleChange}></textarea>
+                        <label for="postContent">Content:</label>
+                    </div>
+        
+                    <button className="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+                </form>
             </div>
-            <div className="input-field">
-                <textarea id="postContent" className="materialize-textarea" data-length="500"></textarea>
-                <label for="postContent">Content:</label>
-            </div>
+        );
+    }
+} 
 
-            <button className="btn waves-effect waves-light" type="submit" name="action">Submit</button>
-        </form>
-    </div>
-)
+const mapStateToProps = (state) => {
+    return {
+        
+    }
+}
 
-export default CreateNewPost;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createPost: post => dispatch(createPost(post))   
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateNewPost);
