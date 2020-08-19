59a564c115dd600fac35573024a69c9cd4bab700
Squirtle <tienhoang.pham@bc.fi>
2020-08-19 10:08:16 +0300

    refactor reducer to use switch case

--- src/store/reducers/rootReducers.js
+++ src/store/reducers/rootReducers.js
@@ -8,19 +8,23 @@ const initialState = {
 };
 
 const rootReducers = (state = initialState, action) => {
-    console.log(state.posts);
-
-    if (action.type === 'CREATE_NEW_POST') {
-        return {
-            ...state,
-            posts: [
-                ...state.posts,
-                action.post
-            ]
-        };
+    switch(action.type) {
+        case 'CREATE_NEW_POST':
+            return {
+                ...state,
+                posts: [
+                    ...state.posts,
+                    action.post
+                ]
+            };
+        case 'REMOVE_ALL_POSTS':
+            return {
+                ...state,
+                posts: []
+            }
+        default: 
+            return state;
     }
-
-    return state; 
 }
 
 export default rootReducers;
